f683c324f78bcb083c30bb4489642f660e78bfd7
nguyenanhdung281101 <nguyenanhdung2811@gmail.com>
2022-11-02 17:16:48 +0700

    base for auth screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
+// Toast
+import store from "app/store";
+import AppToast from "general/components/AppToast";
+import { Suspense, useEffect } from "react";
+import "react-toastify/dist/ReactToastify.css";
+// router
+// import HomeScreen from "features/LandingPage/screens/HomeScreen";
+import AppDialog from "general/components/AppDialog";
+import AppNotFound from "general/components/AppNotFound";
+// import { injectStore } from "general/helpers/WebsocketHelper";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignInScreen from "features/Auth/SignInScreen";
+
+
+// Load BS
+
+require("bootstrap/dist/js/bootstrap.min");
+
+
+// Lazy load - Code splitting
+
+const sTag = "[App]";
 
 function App() {
+  // MARK: --- Hooks ---
+  useEffect(() => {
+    console.log(`${sTag} did load`);
+    // injectStore(store);
+
+    return () => {
+      console.log(`${sTag} will dismiss`);
+    };
+  }, []);
+  // const auth = useSelector((state) => state?.auth?.loggedIn);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* Router */}
+      {/* <BrowserRouter> */}
+      <BrowserRouter>
+        {/* Suspense */}
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            {/* Landing Page */}
+            {/* <Route path="" element={<HomeScreen />} /> */}
+            {/* Dashboard */}
+            {/* <Route
+              path="dashboard/*"
+              element={
+                auth ? (
+                  <PrivateRoute>
+                    <DashboardScreen />
+                  </PrivateRoute>
+                ) : (
+                  <HomeScreen />
+                )
+              }
+            /> */}
+
+            {/* Account */}
+            {/* <Route
+              path="account/*"
+              element={
+                <PrivateRoute>
+                  <Account />
+                </PrivateRoute>
+              }
+            /> */}
+            {/* Sign in */}
+            <Route
+              path="/sign-in"
+              element={
+                // <GuestRoute>
+                  <SignInScreen />
+                // </GuestRoute>
+              }
+            />
+            {/* Sign up */}
+            {/* <Route
+              path="/sign-up"
+              element={
+                <GuestRoute>
+                  <SignUpScreen />
+                </GuestRoute>
+              }
+            /> */}
+            {/* forgot pass */}
+            {/* <Route
+              path="/forgot-pass"
+              element={
+                <GuestRoute>
+                  <ForgotPasswordScreen />
+                </GuestRoute>
+              }
+            /> */}
+            {/* reset pass */}
+            {/* <Route
+              path="/reset-pass"
+              element={
+                <GuestRoute>
+                  <ResetPasswordScreen />
+                </GuestRoute>
+              }
+            /> */}
+            {/* Not Found */}
+            <Route path="*" element={<AppNotFound />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+      {/* </BrowserRouter> */}
+
+      {/* App Dialog */}
+      <AppDialog />
+      {/* Toast */}
+      <AppToast />
+      {/* Listener */}
+      {/* <DataCommonListener /> */}
+      {/* Account Listener */}
+      {/* <AccountListener /> */}
+      {/* <DashboardListener /> */}
+      {/* Firebase Listener */}
+      {/* <FirebaseListener /> */}
+    </>
   );
 }
 
---
+++ src/features/Auth/SignUpScreen/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import HeaderLandingPage from 'general/components/HeaderLandingPage';
+
+SignUpScreen.propTypes = {
+    
+};
+
+function SignUpScreen(props) {
+    return (
+        <div>
+            <HeaderLandingPage />
+            Sign Up Screen
+        </div>
+    );
+}
+
+export default SignUpScreen;
