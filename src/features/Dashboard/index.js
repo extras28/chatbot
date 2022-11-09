08f080bbcf2d47a16d7b77aa403a5350c9477885
nguyenanhdung281101 <nguyenanhdung2811@gmail.com>
2022-11-09 23:09:59 +0700

    update

--- src/features/Dashboard/index.js
+++ src/features/Dashboard/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import EmptyView from 'general/components/EmptyView';
 import HeaderLandingPage from 'general/components/HeaderLandingPage';
 import SideBar from 'general/components/SideBar';
 import Argument from 'general/components/Argument';
+import './style.scss'
 
 Dashboard.propTypes = {
     
@@ -11,11 +11,11 @@ Dashboard.propTypes = {
 
 function Dashboard(props) {
     return (
-        <div>
+        <div className='Dashboard'>
             <HeaderLandingPage loggedIn={true} />
             <div className=''>
                 <SideBar />
-                <div className='ml-50'>
+                <div className='Dashboard_Content pt-10'>
                     <Argument />
                 </div>
             </div>
