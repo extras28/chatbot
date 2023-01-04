8e3501f4324e5263e42769740f43c5f694820315
dxpnkil <dungngocpham2k1@gmail.com>
2023-01-05 00:03:34 +0700

    thêm tab trong profile

--- src/features/Profile/index.js
+++ src/features/Profile/index.js
@@ -5,10 +5,19 @@ import avatar from "../../assets/images/avatar.png";
 import SideBar from "general/components/SideBar";
 import "./style.scss";
 import BaseLayout from "general/components/BaseLayout";
+import { BrowserRouter as Router, Link, useLocation } from "react-router-dom";
 
 Profile.propTypes = {};
 
+function useQuery() {
+  const { search } = useLocation();
+
+  return React.useMemo(() => new URLSearchParams(search), [search]);
+}
+
 function Profile() {
+  let query = useQuery();
+
   return (
     <div className="Profile">
       {/* <HeaderLandingPage loggedIn={true} /> */}
@@ -49,86 +58,127 @@ function Profile() {
           <div className="mt-5 ">
             <ul className="nav nav-pills">
               <li className="nav-item nav-tab">
-                <a
+                <Link
                   className="nav-link rounded-pill active"
-                  aria-current="page"
-                  href="#"
+                  to="/profile?tab=Profile"
                 >
                   Profile
-                </a>
+                </Link>
               </li>
-              <li className="nav-item">
-                <a className="nav-link rounded-pill" href="#">
+              <li className="nav-item nav-tab">
+                <Link
+                  className="nav-link rounded-pill active"
+                  to="/profile?tab=Activity"
+                >
                   Activity
-                </a>
+                </Link>
               </li>
-              <li className="nav-item">
-                <a className="nav-link rounded-pill" href="#">
+              <li className="nav-item nav-tab">
+                <Link
+                  className="nav-link rounded-pill active"
+                  to="/profile?tab=Saves"
+                >
                   Saves
-                </a>
+                </Link>
               </li>
-              <li className="nav-item">
-                <a className="nav-link rounded-pill" href="#" aria-disabled="true">
+              <li className="nav-item nav-tab">
+                <Link
+                  className="nav-link rounded-pill active"
+                  to="/profile?tab=Settings"
+                >
                   Settings
-                </a>
+                </Link>
               </li>
             </ul>
-          </div>
-  
-          <div className="d-flex flex-wrap mt-5">
-            <div className="col-3">
-              <div className="fs-4">Stats</div>
-              <div className="d-flex flex-wrap rounded-2 border p-3">
-                <div className="col">
-                  <p>1</p>
-                  <p>reputation</p>
-                </div>
-                <div className="col">
-                  <p>7</p>
-                  <p>reached</p>
-                </div>
-                <div className="col">
-                  <p>2</p>
-                  <p>answers</p>
-                </div>
-                <div className="col">
-                  <p>1</p>
-                  <p>questions</p>
-                </div>
-              </div>
-            </div>
-            <div className="col">
-              <div className="">
-                <div className="fs-4">About</div>
-                <div className="container d-flex align-items-center justify-content-center bg-light blank rounded-2 border">
-                  <div>Your about me section is currently blank.</div>
-                  <div>
-                    Would you like to add one? <a href="#">Edit profile</a>
-                  </div>
-                </div>
-              </div>
-              <div className="">
-                <div className="fs-4">Badges</div>
-                <div className="container d-flex align-items-center justify-content-center bg-light blank rounded-2 border">
-                  You have not earned any  <a href="#"> badges</a> .
-                </div>
-              </div>
-              {/* <div className="">
-                <div className="fs-4">Post</div>
-                <div className="container d-flex align-items-center justify-content-center bg-light text-wrap blank">
-                  Just getting started? Try answering a question! Your most
-                  helpful questions, answers and tags will appear here. Start by{" "}
-                  <a href="#">answering a question</a> or{" "}
-                  <a href="#">selecting tags</a> that match topics you’re
-                  interested in.
-                </div>
-              </div> */}
-            </div>
+
+            <Child tab={query.get("tab")} />
           </div>
         </div>
       </BaseLayout>
     </div>
   );
 }
+
+function Child({ tab }) {
+  return (
+    <div>
+      {(() => {
+        switch (tab) {
+          case "Profile":
+            return <ProfileUser />;
+          case "Activity":
+            return <Activity />;
+          case "Saves":
+            return <Saves />;
+          case "Settings":
+            return <Settings />;
+          default:
+            return <ProfileUser />;
+        }
+      })()}
+    </div>
+  );
+}
+
+function ProfileUser() {
+  return (
+    <div className="d-flex flex-wrap mt-5">
+      <div className="col-3">
+        <div className="fs-4">Stats</div>
+        <div className="d-flex flex-wrap rounded-2 border p-3">
+          <div className="col">
+            <p>1</p>
+            <p>reputation</p>
+          </div>
+          <div className="col">
+            <p>7</p>
+            <p>reached</p>
+          </div>
+          <div className="col">
+            <p>2</p>
+            <p>answers</p>
+          </div>
+          <div className="col">
+            <p>1</p>
+            <p>questions</p>
+          </div>
+        </div>
+      </div>
+      <div className="col">
+        <div className="">
+          <div className="fs-4">About</div>
+          <div className="container d-flex align-items-center justify-content-center bg-light blank rounded-2 border">
+            <div>Your about me section is currently blank.</div>
+            <div>
+              Would you like to add one? <a href="#">Edit profile</a>
+            </div>
+          </div>
+        </div>
+        <div className="">
+          <div className="fs-4">Badges</div>
+          <div className="container d-flex align-items-center justify-content-center bg-light blank rounded-2 border">
+            You have not earned any <a href="#"> badges</a> .
+          </div>
+        </div>
+        {/* <div className="">
+      <div className="fs-4">Post</div>
+      <div className="container d-flex align-items-center justify-content-center bg-light text-wrap blank">
+        Just getting started? Try answering a question! Your most
+        helpful questions, answers and tags will appear here. Start by{" "}
+        <a href="#">answering a question</a> or{" "}
+        <a href="#">selecting tags</a> that match topics you’re
+        interested in.
+      </div>
+    </div> */}
+      </div>
+    </div>
+  );
+}
+
+function Settings() {}
+
+function Saves() {}
+
+function Activity() {}
 
 export default Profile;
