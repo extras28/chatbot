3e79a526ce00a57052c57c0675e522b4d9bda114
nguyenanhdung281101 <nguyenanhdung2811@gmail.com>
2023-01-04 23:05:00 +0700

    account overview screen

--- src/general/components/AppTabs/index.js
+++ src/general/components/AppTabs/index.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import React, { useState, useEffect } from "react";
+import { Tabs } from "react-bootstrap";
+import { v4 as uuidv4 } from "uuid";
 
-import "./index.scss";
-
+import "./style.scss";
 
 /**
  * @props className (string): không bắt buộc
@@ -18,49 +18,50 @@ function AppTabs(props) {
 
     const onClick = (tab) => {
         props.handleClick(tab);
-    }
+    };
 
-    useEffect(() => {
-        var allTabs = document.getElementById(id).getElementsByClassName('tab');
-        for (var i = 0, len = allTabs.length; i < len; i++) {
-            allTabs[i].addEventListener("click", function () {
-                if (this.classList.contains('active')) return;
+    // useEffect(() => {
+    //     var allTabs = document.getElementById(id).getElementsByClassName("tab");
+    //     for (var i = 0, len = allTabs.length; i < len; i++) {
+    //         allTabs[i].addEventListener("click", function () {
+    //             if (this.classList.contains("active")) return;
 
-                var parent = this.parentNode,
-                    innerTabs = parent.querySelectorAll('.tab');
+    //             var parent = this.parentNode,
+    //                 innerTabs = parent.querySelectorAll(".tab");
 
-                for (var index = 0, iLen = innerTabs.length; index < iLen; index++) {
-                    innerTabs[index].classList.remove('active');
-                }
+    //             for (
+    //                 var index = 0, iLen = innerTabs.length;
+    //                 index < iLen;
+    //                 index++
+    //             ) {
+    //                 innerTabs[index].classList.remove("active");
+    //             }
 
-                this.classList.add('active');
-            });
-        };
-    }, []);
-
+    //             this.classList.add("active");
+    //         });
+    //     }
+    // }, []);
 
     return (
-        <div className={'Tabs_Container ' + props.className} id={id}>
-            <div className='tabbed round d-flex flex-row-reverse justify-content-end'>
-                {tabs.map((text, index) => {
+        <div className='AppTabs rounded bg-gray-200 d-flex flex-stack flex-wrap mb-9 p-2'>
+            <ul className='nav flex-wrap border-transparent'>
+                {tabs.map((item, index) => {
                     return (
-                        <div
-                            key={index}
-                            className={text == activeTab
-                                ? 'tab active'
-                                : 'tab'}
-                            onClick={(e) => {
-                                e.preventDefault();
-                                onClick(text);
-                            }}
-                            style={index == (tabs.length - 1) ? { padding: '0.7rem 1.8rem 0.7rem 1.2rem' } : {}}
-                        // style={index == (tabs.length - 1) ? { padding: '0.7rem 2.4rem 0.7rem 1rem' } : {}}
-                        >
-                            {text}
-                        </div>
+                        <li key={index} className='nav-item my-1'>
+                            <div
+                                className={`btn btn-sm btn-color-gray-600 bg-state-body btn-active-color-gray-800 fw-bolder fw-bold fs-6 fs-lg-base nav-link px-3 px-lg-4 mx-1 ${
+                                    item == activeTab ? "tab active" : "tab"
+                                }`}
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    onClick(item);
+                                }}>
+                                {item}
+                            </div>
+                        </li>
                     );
                 })}
-            </div>
+            </ul>
         </div>
     );
 }
