1ba74606227783278cc5b72d6c40b9c15e9dc2ca
nguyenanhdung281101 <nguyenanhdung2811@gmail.com>
2023-01-13 14:12:05 +0700

    update

--- src/features/Question/Component/SummaryQuestion/index.js
+++ src/features/Question/Component/SummaryQuestion/index.js
@@ -65,18 +65,18 @@ function SummaryQuestion(props) {
                     </div>
                     <div className='d-flex'>
                         <button className='btn'>
-                            <i className='far fa-comment'></i>
+                            <i className='fas fa-comment'></i>
                             {comments}
                         </button>
                         <button className='btn'>
-                            <i className='far fa-thumbs-up'></i>
+                            <i className='fas fa-thumbs-up'></i>
                             {likes}
                         </button>
                         <button className='btn'>
-                            <i className='far fa-thumbs-down'></i>
+                            <i className='fas fa-thumbs-down'></i>
                             {dislikes}
                         </button>
-</div>
+                    </div>
                 </div>
             </div>
         </div>
